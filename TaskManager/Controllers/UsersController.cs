dc799ca1e0d1e3f5050550f2ab769e795bc9992e
Kuznietsov-hub <batyagame1@gmail.com>
2023-03-12 12:01:43 +0300

    Add project files.

---
+++ TaskManager/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+	public class UsersController : Controller
+	{
+		public IActionResult NewUser()
+		{
+			return View();
+		}
+	}
+}
